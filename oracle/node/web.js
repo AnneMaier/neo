4cea67612f760dfd3201f26c82e98ae697ffe38a
AnneMaier <hhch98@gmail.com>
2025-03-28 14:09:24 +0900

    MOD node.js web file

--- oracle/node/web.js
+++ oracle/node/web.js
@@ -4,58 +4,107 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-oracledb.initOracleClient({ libDir: '/opt/oracle/instantclient_21_13' });
+oracledb.initOracleClient({ libDir: '/opt/oracle/instantclient_21_13' })
 
 var app = express();
-app.set('port', process.env.PORT || 3000); // 'Port'를 'port'로 수정
-app.use(bodyParser.urlencoded({ extended: true })); // app.set() 대신 app.use() 사용
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/dbTestSelect', function(req, res) { // seq를 res로 수정
+oracledb.autoCommit = true;
+
+app.get('/', (req, res) => {
+    res.send('Web Server started~!!');
+})
+
+app.get('/dbTestSelect', function (req, res) {
     oracledb.getConnection(
         {
             user: dbConfig.user,
             password: dbConfig.password,
             connectString: dbConfig.connectString
-        }, function(err, connection) {
+        },
+        function (err, connection) {
             if (err) {
                 console.error(err.message);
                 return;
             }
-            connection.execute(
-                'SELECT * FROM usertbl', // 'select'를 'SELECT'로 수정
-                function(err, result) {
-                    if (err) {
-                        console.error(err.message);
-                        doRelease(connection, null, res); // res 전달
-                        return;
-                    }
+            let query = "select * from usertbl";
 
-                    console.log(result.rows);
-                    doRelease(connection, result.rows, res); // res 전달
+            connection.execute(query, [], function (err, result) {
+                if (err) {
+                    console.err(err.message);
+                    doRelease(connection);
+                    return;
                 }
-            );
+                console.log(result.rows);
+                doRelease(connection, result.rows);
+            })
+        })
 
-            function doRelease(connection, rowList, res) { // res 인자 추가
-                connection.release(
-                    function(err) {
-                        if (err) {
-                            console.error(err.message);
-                        }
-                        if (rowList) {
-                            console.log('List size: ' + rowList.length);
-                            res.send(rowList); // 응답 전송
-                        }
-                    }
-                );
+    function doRelease(connection, rowList) {
+        connection.release(
+            function (err) {
+                if (err) {
+                    console.error(err.message);
+                }
+                console.log('List size : ' + rowList.length);
+                res.send(rowList);
             }
-        }
-    );
+        )
+    }
 });
 
-app.all('*', function(req, res) {
-    res.status(404).send('<h1>404 Not found</h1>'); // 400을 404로 수정
-});
+app.post('/dbTestInsert', function (req, res) {
+    oracledb.getConnection({
+        user: dbConfig.user,
+        password: dbConfig.password,
+        connectString: dbConfig.connectString
+    },
+        function (err, connection) {
+            if (err) {
+                console.error(err.message);
+                return;
+            }
+            let query = 'insert into usertbl(userid, username, birthyear, addr, mobile1, mobile2, height, mdate) ' +
+                'values(:userid, :username, :birthyear, :addr, :mobile1, :mobile2, :height, :mdate)';
 
-app.listen(app.get('port'), function() {
-    console.log('3000 Port: server started~!');
-});
+            let binddata = [
+                req.body.userid,
+                req.body.username,
+                Number(req.body.birthyear),
+                req.body.addr,
+                req.body.mobile1,
+                req.body.mobile2,
+                Number(req.body.height),
+                new Date(req.body.mdate)
+            ];
+
+            connection.execute(query, binddata, function (err, result) {
+                if (err) {
+                    console.error(err.message);
+                    doRelease(connection);
+                    return;
+                }
+                console.log('Row Insert : ' + result.rowsAffected);
+                doRelease(connection, result.rowsAffected);
+            });
+        });
+
+    function doRelease(connection, result) {
+        connection.release(function (err) {
+            if (err) {
+                console.error(err.message);
+            }
+            res.sendStatus(200);
+        })
+    }
+})
+
+app.all('*', function (req, res) {
+    res.status(404).send('<h1>Error - Page is not found.</h1>');
+})
+
+app.listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
+})
