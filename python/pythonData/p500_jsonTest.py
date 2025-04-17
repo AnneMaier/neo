73034d8dc1e5bfcac8f62c215b8a1356a68e2a08
hhch <hhch98@gmail.com>
2025-04-17 15:32:28 +0900

    ADD API study

--- python/pythonData/p500_jsonTest.py
+++ python/pythonData/p500_jsonTest.py
@@ -1,23 +1,49 @@
 import json
-data = {'age': 30, 'name': '홍길동', 'address': '마포구 공덕동',
-        'broadcast':{
-            'sbs': 5, 'kbs': 9, 'mbc': 11
-        }
-}
 
-json_str = json.dumps(data, ensure_ascii=False, indent=4, sort_keys=True)
-print(json_str)
-print(type(json_str))
-print('-' * 50)
+def get_Json_data():
+    filename = 'jumsu.json'
+    myfile = open(filename, 'r', encoding='utf-8')
+    print(type(myfile))
+    print(myfile)
+    print('-' * 50)
+    
+    myfile = myfile.read()
+    print(type(myfile))
+    print(myfile)
+    print('-' * 50)
+    
+    jsondata = json.loads(myfile)
+    print(type(jsondata))
+    print(jsondata)
+    print('-' * 50)
 
-json_data = json.loads(json_str)
-print(json_data)
-print(type(json_data))
-print('-' * 50)
+    for oneitem in jsondata:
+        print(oneitem.keys())
+        print(oneitem.values())
+        print('-' * 50)
+        kor = float(oneitem['kor']) 
+        eng = float(oneitem['eng']) 
+        math = float(oneitem['math'])
+        total = kor + eng + math
+        print('국어 : ', kor)
+        print('영어 : ', eng)
+        print('수학 : ', math)
+        print('총점 : ', total)
+        print('-' * 50)
 
-print(json_data['name'])
-print(json_data['age'])
-print(json_data['broadcast']['mbc'])
-print('-' * 50)
+        if 'hello' in oneitem.keys():
+            message = oneitem['hello']
+            print('message : ', message)
 
-print('finished')
+        _gender = oneitem['gender'].upper()
+
+        if _gender == 'M':
+            print('성별 : ', _gender)
+        elif _gender == 'F':
+            print('성별 : ', _gender)
+        else:
+            print('성별 정보가 없습니다.')
+    print('-' * 50)
+    
+if __name__ == '__main__':
+    get_Json_data()
