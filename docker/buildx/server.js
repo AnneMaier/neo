b554c36042db60125a200a69a143100f7fc1e0d8
AnneMaier <hhch98@gmail.com>
2025-05-08 14:43:37 +0900

    ADD dockerfile test 3

---
+++ docker/buildx/server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var http = require('http');
+const hostname = '0.0.0.0'
+const port = 8000
+
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type', 'text/plain' });
+    res.end(`Hello from NodeJS in ${process.arch}\n`);
+});
+
+server.listen(port, hostname, function () {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
