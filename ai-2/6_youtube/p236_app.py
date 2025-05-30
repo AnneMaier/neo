6b63817aed3def87bf2a18d2f498a3eb90d32ef9
AnneMaier <hhch98@gmail.com>
2025-05-30 06:33:43 +0000

    study youtube 활용 기술술

---
+++ ai-2/6_youtube/p236_app.py
@@ -0,0 +1,12 @@
+def get_video_id(video_url):
+    video_id = video_url.split('v=')[1][:11]
+
+    return video_id
+
+video_url = 'https://www.youtube.com/watch?v=pSJrML-TTmI'
+print("YouTube Video ID : ", get_video_id(video_url))
+
+video_url = "https://www.youtube.com/watch?v=YYXdXT2l-Gg&list=PL-osiE80TeTt2d9bfVyTiXJA-UTHn6WwU&index=1"
+print("YouTube Video ID : ", get_video_id(video_url))
+
+# 유튜브 자막 정보 가져오기
