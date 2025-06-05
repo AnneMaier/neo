6ba5f39b541edd370b10913e37df6db08ef13b27
AnneMaier <hhch98@gmail.com>
2025-06-05 07:00:01 +0000

    openai with ollama

--- ai-3/ollama/1_app.py
+++ ai-3/ollama/1_app.py
@@ -1,7 +1,7 @@
 import ollama
 
 response = ollama.chat(
-    model = 'llama3.2:latest', 
+    model = 'llama3:latest', 
     messages = [
         {'role' : 'system', 'content' : 'You are a python expert.' },
         {'role' : 'user', 'content' : 'Code a python function to generate a Fibonacci sequence.' }
--- ai-3/ollama/2_app.py
+++ ai-3/ollama/2_app.py
@@ -1,12 +1,16 @@
 from openai import OpenAI
+import os
+
+
+ollama_host = os.environ["OLLAMA_HOST"]
 
 client = OpenAI(
-    base_url = 'http://localhost:11434/v1',
+    base_url = f'{ollama_host}/v1',
     api_key='ollama',
 )
 
 response = client.chat.completions.create(
-    model = 'llama3.2::latest',
+    model = 'llama3:latest',
     messages = [
         {'role': 'system', 'content': 'You are a python expert. '},
         {'role': 'user', 'content': 'Code a Python function to generate a Fibonacci sequence.'}
